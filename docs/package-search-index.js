1210186d326c7ff742c794d4d4eb4c4cf3303f9c
Rémy Dornier <remy.dornier@epfl.ch>
2026-05-27 15:18:34 +0200

    update javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"qupath.ext.biop.cellpose"},{"l":"qupath.ext.biop.cmd"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"qupath.ext.biop.cellpose"},{"l":"qupath.ext.biop.cmd"}];updateSearchResults();
